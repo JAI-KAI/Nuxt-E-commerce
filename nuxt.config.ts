e8ad1a5d1c4757b35829aaf8d29b2c77e93dffc7
JAI-KAI <good003047@gmail.com>
2025-03-28 07:55:34 +0800

    color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  ui: {
+    colorMode: false
+  }
 })
