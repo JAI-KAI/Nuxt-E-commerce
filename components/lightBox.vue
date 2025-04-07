e254bd9374c8ea3e5613f173a2087bc2db861c32
JAI-KAI <good003047@gmail.com>
2025-04-07 20:33:13 +0800

    燈箱修正

--- components/lightBox.vue
+++ components/lightBox.vue
@@ -2,13 +2,11 @@
     <div class="fixed top-5 left-1/2 -translate-x-1/2 z-50">
         <transition-group 
             name="fade" 
-            tag="div"
-            class="space-y-2 relative"
-            :class="{' bg-amber-950': isAllLeaving.value, 'bg-white': !isAllLeaving.value}">
+            tag="div" 
+            class="space-y-2">
             <div v-for="msg of messages" 
                 :key="msg.id"
-                class=" px-6 py-3 rounded shadow-md text-center min-w-[240px]"
-                >
+                class=" bg-white px-6 py-3 rounded shadow-md text-center min-w-[240px]">
                 {{ msg.text }}
             </div>
         </transition-group>
@@ -22,7 +20,7 @@ const { messages } = useMessage();
 const isAllLeaving = ref(false);
 
 watchEffect(() => {
-  isAllLeaving.value = messages.value.length === 0;
+  isAllLeaving.value = messages.value.length === 1;
   console.log(isAllLeaving.value);
 });
 </script>
@@ -31,7 +29,7 @@ watchEffect(() => {
 .fade-move,
 .fade-enter-active,
 .fade-leave-active {
-    transition: all 5s ease;
+    transition: all 0.5s ease;
 }
 
 .fade-enter-from {
@@ -44,8 +42,9 @@ watchEffect(() => {
     transform: translateY(-30px);
 }
 
-.fade-leave-active {
+/* 有bug會造成元素偏移 */
+/* .fade-leave-active {
     position: absolute;
-    /* left: -120px; */
-}
+    :class="{'left-[-120px]': isAllLeaving}"
+} */
 </style>
