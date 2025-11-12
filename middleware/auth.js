05088308ddc7767f5bec7ad9498575b54337bd25
JAI-KAI <good003047@gmail.com>
2025-11-12 15:28:29 +0800

    修正中介邏輯

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,18 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    if (import.meta.client) {
-        // 從 Composable 中獲取狀態
-        const { isLoggedIn } = useAuth()
+    const { addMessage } = useMessage()
 
-        // 防止已登入用戶回到登入頁面
-        if (to.path === '/login' && isLoggedIn.value) {
-            alert('您已登入，將為您導航回首頁')
-            return navigateTo('/')
-        }
+    // 從 Composable 中獲取狀態
+    const { isLoggedIn } = useAuth()
 
-        // 防止未登入用戶訪問受保護頁面
-        if (to.path === '/' && !isLoggedIn.value) {
-            alert('請先登入以繼續操作')
-            return navigateTo('/login')
-        }
+    // 防止已登入用戶回到登入頁面
+    if (to.path === '/login' && isLoggedIn.value) {
+        addMessage('您已登入，將為您導航回首頁')
+        return navigateTo('/')
+    }
+
+    // 防止未登入用戶訪問受保護頁面
+    if (to.path === '/' && !isLoggedIn.value) {
+        addMessage('請先登入以繼續操作')
+        return navigateTo('/login')
     }
 })
